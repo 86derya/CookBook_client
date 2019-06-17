939882830bda49839dd80a38f41bb33e588e4593
Andrii Dederia <derya_86@ukr.net>
2019-06-18 02:50:54 +0300

    eslint plugin

--- src/configs/serverUrl.js
+++ src/configs/serverUrl.js
@@ -1,3 +1,3 @@
-const serverURL = 'https://dederiacookbookserver.herokuapp.com/';
+const serverURL = 'https://dederiacookbookserver.herokuapp.com';
 // const serverURL = 'http://localhost:8080';
 export default serverURL;
